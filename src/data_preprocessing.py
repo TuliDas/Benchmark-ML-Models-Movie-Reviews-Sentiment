5ce73a66e9d4352cd01165fa3a5cfc6a236cd009
Tuli Rani Das <tuli.r.das@gmail.com>
2025-08-17 13:41:34 +0600

    Update data_preprocessing.py

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -71,11 +71,12 @@ def preprocess_pipeline(text,use_stemming=False):
     text = lowercase_and_clean(text)
     text = handle_negations(text)
     #text = remove_white_space(text)
-    text = lemmatize_texts(text)
+    text = lemmatize_text(text)
     text = remove_stopwords(text)
     text = remove_short_tokens(text)
     if use_stemming:
         text= stem_text(text)
     
     return text 
+
 
