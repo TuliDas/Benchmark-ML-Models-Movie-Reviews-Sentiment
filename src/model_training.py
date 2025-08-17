9c88369b7f5336e44b3f57ff874bdf86d10c560c
Tuli Rani Das <tuli.r.das@gmail.com>
2025-08-17 16:49:21 +0600

    Update model_training.py

--- src/model_training.py
+++ src/model_training.py
@@ -1,33 +1,26 @@
 from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
 
+def model_train(model):
+    """
+    Train a given model on both Bag of Words (BoW) and TF-IDF features, 
+    then return predictions on the test set.
 
-def train_logistic_regression(bow_reviews_train, tfidf_reviews_train ,sentiment_train, bow_reviews_test, 
-                                                                            tfidf_reviews_test):
-    lr = LogisticRegression(penalty='l2', max_iter=500, C=1, random_state=42)
-    
-    #Fitting the model for Bag of Words
-    lr_bow = lr.fit(bow_reviews_train,sentiment_train)
+    Args:
+        model: sklearn classifier instance (e.g., LogisticRegression, MultinomialNB, etc.)
+    Returns:
+        pred_for_bow (np.ndarray): Predictions on test set using BoW features.
+        pred_for_tfidf (np.ndarray): Predictions on test set using TF-IDF features.
+    """
 
-    #Fitting the model for TF-IDF features
-    lr_tfidf = lr.fit(tfidf_reviews_train,sentiment_train)
-    
+    # Train separately
+    model_bow = model.fit(bow_reviews_train, sentiment_train)
+    model_tfidf = model.fit(tfidf_reviews_train, sentiment_train)
 
-    #Predicting the model for bag of words & TF-IDF features
-    lr_bow_pred = lr.predict(bow_reviews_test)
-    lr_tfidf_pred = lr.predict(tfidf_reviews_test)
+    # Predict on test data
+    pred_for_bow = model_bow.predict(bow_reviews_test)
+    pred_for_tfidf = model_tfidf.predict(tfidf_reviews_test)
 
-    return lr,lr_bow_pred, lr_tfidf_pred 
-
-
-
-def train_multinomial_nb():
-    return
-
-def train_sgd():
-    return
-
-def train_svc():
-    return
-
+    return pred_for_bow, pred_for_tfidf
